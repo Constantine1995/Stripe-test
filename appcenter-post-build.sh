01c116148173aa63ce594ed87f581d7610e07b43
Константин Лихачёв <constantine.lord.darkness@gmail.com>
2019-11-01 20:19:29 +0200

    fixed post-build

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -7,7 +7,7 @@ if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
      then
         curl \
         -F "status=2" \
-        -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/MyApps.ipa" \
+        -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/Stripe-test.ipa" \
         -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" \
         https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions/upload
     else
