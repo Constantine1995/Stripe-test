79f35fe82badf8227dc69eb6190eb7781cea4ce5
Константин Лихачёв <constantine.lord.darkness@gmail.com>
2019-11-01 21:45:19 +0200

    return files

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Example: Clone a required repository
-git clone https://github.com/example/SomeProject
+git clone https://github.com/Constantine1995/Stripe-test
 
 # Example: Install App Center CLI
 npm install -g appcenter-cli
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,5 +3,19 @@
 # Example: Change bundle name of an iOS app for non-production
 if [ "$APPCENTER_BRANCH" != "master" ];
 then
-plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/MyApp/Info.plist
+plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/Stripe-test/Info.plist
 fi
+
+CUR_COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' ios/Podfile.lock`
+ENV_COCOAPODS_VER=`pod --version`
+
+# check if not the same version, reinstall cocoapods version to current project's
+if [ $CUR_COCOAPODS_VER != $ENV_COCOAPODS_VER ];
+then
+echo "Uninstalling all CocoaPods versions"
+sudo gem uninstall cocoapods --all --executables
+echo "Installing CocoaPods version $CUR_COCOAPODS_VER"
+sudo gem install cocoapods -v $CUR_COCOAPODS_VER
+else
+echo "CocoaPods version is suitable for the project"
+fi;
